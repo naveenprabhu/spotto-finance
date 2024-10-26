a0cf359331274dfe90feec7ec5955be3470e78f5
Naveen Arumugam <naveenprabhu.a@gmail.com>
2024-10-26 18:48:39 +1100

    Add loan health check

--- src/Loan-Health-Script.js
+++ src/Loan-Health-Script.js
@@ -1,11 +1,16 @@
-function showSectionGenerateOtp() {
+alert('Hello');
+function toggleVisibility() {
 	// // Show the hidden section
 	// document.getElementById('sec-4977').style.display = 'block';
 
 	// // Smooth scroll to the section
 	// document.getElementById('sec-4977').scrollIntoView({ behavior: 'smooth' });
 
-	document.getElementById('sec-4976').style.display = 'none';
-	document.getElementById('sec-4977').style.display = 'block';
+	// document.getElementById('sec-4976').style.display = 'none';
+	// document.getElementById('sec-4977').style.display = 'block';
+	const submissionDiv = document.getElementById("carousel_fe60");
+	const thanksDiv = document.getElementById("carousel_417d");
+	submissionDiv.classList.toggle("u-hidden"); // Toggles the visibility class
+	thanksDiv.classList.toggle("u-hidden"); // Toggles the visibility class
 
 }
