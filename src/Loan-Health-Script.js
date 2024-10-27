ad8c8ea4534a76a4ee93db41ab89061f9b835e6d
Naveen Arumugam <naveenprabhu.a@gmail.com>
2024-10-27 18:30:26 +1100

    add check validations

--- src/Loan-Health-Script.js
+++ src/Loan-Health-Script.js
@@ -1,4 +1,43 @@
+function generateOTP() {
+	const generateOTPButton = document.getElementById("generateOtp");
+	// generateOTPButton.classList.remove("u-btn-step-next");
+	// alert("add test");
+	// generateOTPButton.classList.add("u-btn-step-next");
+} 
+
+function verifyOTP() {
+	const verifyOTPButton = document.getElementById("verifyOtp");
+}
+
+function viewResults(viewResultButton) {
+	viewResultButton.classList.remove("u-btn-submit");
+	viewResultButton.classList.remove("u-btn-step-next");
+	const interestRateNumberPattern = /^\d+(\.\d+)?$/;
+	const principalPattern = /^\d+$/; 
+	const principal = parseFloat(document.getElementById("text-7847").value);
+	const annualInterestRate = parseFloat(document.getElementById("text-6839").value);
+
+	if (!interestRateNumberPattern.test(annualInterestRate) || !principalPattern.test(principal) || isNaN(principal) || isNaN(annualInterestRate) || principal <= 0 || annualInterestRate <= 0) {
+		return false;
+	}
+	return true;
+
+
+}
+
 function toggleVisibility() {
+
+	const viewResultButton = document.getElementById("viewResult");
+	if(viewResults(viewResultButton)) {
+		viewResultButton.classList.add("u-btn-submit");
+		const submissionDiv = document.getElementById("carousel_fe60");
+		const thanksDiv = document.getElementById("carousel_417d");
+		submissionDiv.classList.toggle("u-hidden"); // Toggles the visibility class
+		thanksDiv.classList.toggle("u-hidden"); // Toggles the visibility class
+		calculateMonthlyRepayment();
+		calculatePotentialRepaymentAmount();
+	}
+
 	// // Show the hidden section
 	// document.getElementById('sec-4977').style.display = 'block';
 
@@ -8,12 +47,7 @@ function toggleVisibility() {
 	// document.getElementById('sec-4976').style.display = 'none';
 	// document.getElementById('sec-4977').style.display = 'block';
 	
-	const submissionDiv = document.getElementById("carousel_fe60");
-	const thanksDiv = document.getElementById("carousel_417d");
-	submissionDiv.classList.toggle("u-hidden"); // Toggles the visibility class
-	thanksDiv.classList.toggle("u-hidden"); // Toggles the visibility class
-	calculateMonthlyRepayment();
-	calculatePotentialRepaymentAmount();
+
 
 }
 
@@ -22,6 +56,7 @@ function calculateMonthlyRepayment() {
 	const principal = parseFloat(document.getElementById("text-7847").value);
 	const annualInterestRate = parseFloat(document.getElementById("text-6839").value);
 	const loanTermYears = 30;  // Fixed loan term of 30 years
+
 	
 	// Validate input values
 	if (isNaN(principal) || isNaN(annualInterestRate) || principal <= 0 || annualInterestRate <= 0) {
@@ -36,9 +71,10 @@ function calculateMonthlyRepayment() {
 	// Calculate the monthly repayment using the loan amortization formula
 	const monthlyRepayment = (principal * monthlyInterestRate * Math.pow(1 + monthlyInterestRate, numberOfPayments)) / 
 													 (Math.pow(1 + monthlyInterestRate, numberOfPayments) - 1);
+
 	
 	// Display the monthly repayment rounded to two decimal places
-	document.getElementById("monthlyRepayment").textContent = `$${monthlyRepayment.toFixed(2)}/month`;
+	document.getElementById("monthlyRepayment").textContent = `${formatCurrency(monthlyRepayment.toFixed(2))}/month`;
 	return monthlyRepayment;
 }
 
@@ -61,7 +97,7 @@ function calculatePotentialRepaymentAmount() {
 	const numberOfPayments = loanTermYears * 12;
 
 	// Calculate the monthly repayment using the loan amortization formula
-	const potentialMonthlyRepayment = (principal * monthlyInterestRate * Math.pow(1 + monthlyInterestRate, numberOfPayments)) / 
+	let potentialMonthlyRepayment = (principal * monthlyInterestRate * Math.pow(1 + monthlyInterestRate, numberOfPayments)) / 
 													 (Math.pow(1 + monthlyInterestRate, numberOfPayments) - 1);
 	
 
@@ -69,19 +105,21 @@ function calculatePotentialRepaymentAmount() {
 		const monthlySavings = monthlyRepayment - potentialMonthlyRepayment;
     const yearlySavings = monthlySavings > 0 ? monthlySavings * 12 : 0;
 
-		document.getElementById("monthlyPotentialRepayment").textContent = `$${potentialMonthlyRepayment.toFixed(2)}/month`;
-
-
     if (yearlySavings > 0) {
-        document.getElementById("savingsAmount").textContent = `By refinancing you could save up to $${yearlySavings.toFixed(2)}/year`;
+        document.getElementById("savingsAmount").textContent = `$${yearlySavings.toFixed(2)}/year`;
     } else {
         // If there are no savings
-        document.getElementById("savingsAmount").textContent = "Your are on the cheapest loan possible, we could discuss about potential cashback options";
-        
-        // Set potential repayment to monthly repayment if it's higher
-        document.getElementById("potentialRepayment").textContent = `Potential Repayment: $${monthlyRepayment.toFixed(2)}`;
+				document.getElementById("savingsAmount").textContent = `$0/year`;
+				potentialMonthlyRepayment = monthlyRepayment
     }
+    
+		// Set potential repayment to monthly repayment if it's higher
+    document.getElementById("monthlyPotentialRepayment").textContent = `${formatCurrency(potentialMonthlyRepayment.toFixed(2))}/month`;
+		const totalSavings = yearlySavings + 3000 + 2000;
+    document.getElementById("totalSavingsAmount").textContent = `Total Potential Savings: ${formatCurrency(totalSavings.toFixed(2))}*`;
 
+}
 
-	
+function formatCurrency(amount) {
+	return `$${amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`;
 }
